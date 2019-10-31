1750c887bab8cac8e96efe4c327050d172a71cd0
tackro <inoue@inouenoiMac.local>
2019-10-31 10:56:42 +0900

    homepageの階層修正

--- src/Form.js
+++ src/Form.js
@@ -4,7 +4,13 @@ export default function Form() {
   return (
     <div className="form_wrap">
       <form>
-        <input type="text" name="desc" className="input_box"></input>
+        <label for="addDoing">作業内容</label>
+        <input
+          type="text"
+          name="desc"
+          className="input_box"
+          id="addDoing"
+        ></input>
         <button type="submit" className="add">
           追加
         </button>
